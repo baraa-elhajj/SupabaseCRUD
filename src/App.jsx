9091b41245cb77d76e9c6295ff7261eee29ae3fd
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-03 22:31:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 import supabase from "./supabase-client";
 
 function App() {
-  return <></>;
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setTodo] = useState("");
+
+  // CRUD functions goes here
+
+  return (
+    <>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="New todo ..."
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button>Add Todo</button>
+      </div>
+      <div>
+        <ul></ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
